8574a2e24aeba5d064fc39e09d0ff9c7e310da60
Corbin A <corbin_amos@yahoo.com>
2020-07-11 12:40:13 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,8 @@
 def turn_count(board)
   counter = 0
-  board.each{|member| counter += 1}
+  for member in board
+    if member == "X"
+      counter += 1
+    if member == "O"
+      counter += 1
 end
