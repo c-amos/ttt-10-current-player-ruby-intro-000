3e0747b72789087e6bc64cae58de7380c731244d
Corbin A <corbin_amos@yahoo.com>
2020-07-11 12:45:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,15 +5,19 @@ def turn_count(board)
   for member in board
     if member == "X"
       counter += 1
+    end
     if member == "O"
       counter += 1
+    end
   return counter
 end
 
 def current_player(board)
   if counter % 2 == 0
     current_player = "X"
+  end
   else
     current_player = "O"
+  end
   return current_player
 end
